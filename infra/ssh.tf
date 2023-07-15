c91c6e3beef6d38cad68c1596c03955afe98209b
HajarAbuRmeileh <hajar.maint@gmail.com>
2023-07-15 21:43:38 +0300

    first commit

--- infra/ssh.tf
+++ infra/ssh.tf
@@ -7,7 +7,7 @@ resource "azapi_resource" "ssh_public_key" {
   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   name      = random_pet.ssh_key_name.id
   location  = "westus3"
-  parent_id = azurerm_resource_group.rg.id
+  parent_id = azurerm_resource_group.example.id
 }
 
 resource "azapi_resource_action" "ssh_public_key_gen" {
